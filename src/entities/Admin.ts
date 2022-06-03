67e6d787cc49a69e983a11ebbb44d5932ce3a205
Matheus <matheus.g.almeida21@gmail.com>
2022-06-03 11:04:42 -0300

    feat: add admin entity

---
+++ src/entities/Admin.ts
@@ -0,0 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity("users_admin")
+export class Admin {
+    @PrimaryGeneratedColumn("uuid")
+    id: string;
+
+    @Column({ type: "varchar", length: 250, unique: true })
+    email: string;
+
+    @Column({ type: "text", select: false })
+    password: string;
+
+    @Column({ type: "text", select: false, name: "admin_key" })
+    adminKey: string;
+}
