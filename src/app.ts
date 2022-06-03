4192be2c9b0237885255d5378a2b48d0641b0b2b
Matheus <matheus.g.almeida21@gmail.com>
2022-06-03 20:30:47 -0300

    fix: add handler error in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 
+import { ErrorHandler, CatchError } from "./errors";
 import { routes } from "./routes";
 
 const app = express();
@@ -7,5 +8,11 @@ const app = express();
 app.use(express.json());
 
 routes(app);
+
+app.use(
+    (err: ErrorHandler, _req: Request, res: Response, _next: NextFunction) => {
+        new CatchError().catch(err, res);
+    }
+);
 
 export default app;
