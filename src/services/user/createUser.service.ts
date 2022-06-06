39dce7ca0373daac143e6b39c2b4d652e9dd5d26
Matheus <matheus.g.almeida21@gmail.com>
2022-06-05 22:01:29 -0300

    refac: move hash password logic to createUser service

--- src/services/user/createUser.service.ts
+++ src/services/user/createUser.service.ts
@@ -1,8 +1,15 @@
+import bcrypt from "bcrypt";
+
 import { UserRepository } from "../../repositories";
 import { IUser } from "../../repositories/user/interfaceUser.repository";
 
 const createUserService = async (userData: IUser) => {
-    const { id, email, ..._ } = await new UserRepository().createUser(userData);
+    const hashedPass = await bcrypt.hash(userData.password, 10);
+
+    const { id, email, ..._ } = await new UserRepository().createUser({
+        ...userData,
+        password: hashedPass,
+    });
 
     return { id, email };
 };
--- src/shapes/user/createUser.shape.ts
+++ src/shapes/user/createUser.shape.ts
@@ -1,12 +1,8 @@
-import bcrypt from "bcrypt";
 import * as yup from "yup";
 
 const createUserShape = yup.object().shape({
     email: yup.string().trim().lowercase().required(),
-    password: yup
-        .string()
-        .transform((pwd) => bcrypt.hashSync(pwd, 10))
-        .required(),
+    password: yup.string().trim().strict().required(),
 });
 
 export { createUserShape };
